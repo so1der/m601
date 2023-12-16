b93c588e60d5c691e39ac36883f47acfd7576784
so1der <john.doe@example.com>
2023-12-16 20:13:52 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+class M601:
+    def __init__(self):
+        pass
+
+    def read_settings(self):
+        pass
+
+    def write_settings(self):
+        pass
+
+    def parse_settings(self):
+        pass
+
+    def make_package(self):
+        pass
+
+    def send_package(self):
+        pass
