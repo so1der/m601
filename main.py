55dfa15ccb9077e9180f637818c86693ab94af7d
so1der <john.doe@example.com>
2023-12-17 10:38:44 +0200

    code imrovement, implement mode change

--- main.py
+++ main.py
@@ -20,62 +20,14 @@ class M601:
 
 
     def read_settings(self):
-        self.dev.ctrl_transfer(
-            0x21,   # bmRequestType
-            0x09,   # bRequest
-            0x0305, # ReportID:5, ReportType:3 (Feature) 
-            1,      # wIndex: 1
-            [5, 1, 0, 0, 0, 0] # the HID payload as a byte array
-        )
-        self.dev.ctrl_transfer(
-            0xa1,   # bmRequestType
-            0x01,   # bRequest
-            0x0305, # ReportID:5, ReportType:3 (Feature) 
-            1,      # wIndex: 1
-            6 # the HID payload as a byte array
-        )
-        self.dev.ctrl_transfer(
-            0x21,   # bmRequestType
-            0x09,   # bRequest
-            0x0305, # ReportID:5, ReportType:3 (Feature) 
-            1,      # wIndex: 1
-            [5, 2, 0, 0, 0, 0] # the HID payload as a byte array
-        )
-        self.dev.ctrl_transfer(
-            0xa1,   # bmRequestType
-            0x01,   # bRequest
-            0x0305, # ReportID:5, ReportType:3 (Feature) 
-            1,      # wIndex: 1
-            6 # the HID payload as a byte array
-        )
-        self.dev.ctrl_transfer(
-            0x21,   # bmRequestType
-            0x09,   # bRequest
-            0x0305, # ReportID:5, ReportType:3 (Feature) 
-            1,      # wIndex: 1
-            [5, 0x11, 0, 0, 0, 0] # the HID payload as a byte array
-        )
-        self.settings_1 = self.dev.ctrl_transfer(
-            0xa1,   # bmRequestType
-            0x01,   # bRequest
-            0x0304, # ReportID:5, ReportType:3 (Feature) 
-            1,      # wIndex: 1
-            520 # the HID payload as a byte array
-        )
-        self.dev.ctrl_transfer(
-            0x21,   # bmRequestType
-            0x09,   # bRequest
-            0x0305, # ReportID:5, ReportType:3 (Feature) 
-            1,      # wIndex: 1
-            [5, 0x21, 0, 0, 0, 0] # the HID payload as a byte array
-        )
-        self.settings_2 = self.dev.ctrl_transfer(
-            0xa1,   # bmRequestType
-            0x01,   # bRequest
-            0x0304, # ReportID:5, ReportType:3 (Feature) 
-            1,      # wIndex: 1
-            520 # the HID payload as a byte array
-        )
+        self.set_report([5, 1, 0, 0, 0, 0])
+        self.get_report(0x0305, 6)
+        self.set_report([5, 2, 0, 0, 0, 0])
+        self.get_report(0x0305, 6)
+        self.set_report([5, 0x11, 0, 0, 0, 0])
+        self.settings_1 = self.get_report(0x0304, 520)
+        self.set_report([5, 0x21, 0, 0, 0, 0])
+        self.settings_2 = self.get_report(0x0304, 520)
         print(self.settings_1)
         print(self.settings_2)
 
@@ -88,14 +40,31 @@ class M601:
     def make_package(self):
         pass
 
-    def send_package(self):
-        pass
+    def set_report(self, payload):
+        self.dev.ctrl_transfer(
+            0x21,   # bmRequestType
+            0x09,   # bRequest
+            0x0305, # ReportID:5, ReportType:3 (Feature) 
+            1,      # wIndex: 1
+            payload # the HID payload as a byte array
+        )
 
-    def get_package(self):
-        pass
+    def get_report(self, wValue, wLength):
+        return self.dev.ctrl_transfer(
+            0xa1,   # bmRequestType
+            0x01,   # bRequest
+            wValue,
+            1,      # wIndex: 1
+            wLength # the length of expected package to read
+        )
+
+    def change_mode(self, mode):
+        if mode not in [1, 2]:
+            raise ValueError('Wrong mode')
+        self.set_report([5, 2, mode, 0, 0, 0])
 
 
 if __name__ == '__main__':
 
     mouse = M601()
-    mouse.read_settings()
+    mouse.change_mode(1)
