8450bf84acbccc86376d91dfe15ce8e6445913a2
so1der <john.doe@example.com>
2023-12-17 18:19:45 +0200

    implement settings parsing

--- main.py
+++ main.py
@@ -28,14 +28,52 @@ class M601:
         self.settings_1 = self.get_report(0x0304, 520)
         self.set_report([5, 0x21, 0, 0, 0, 0])
         self.settings_2 = self.get_report(0x0304, 520)
-        print(self.settings_1)
-        print(self.settings_2)
+        #print(self.settings_1)
+        #print(self.settings_2)
 
     def write_settings(self):
         pass
 
-    def parse_settings(self):
-        pass
+
+    def parse_settings(self, settings):
+        self.raw_polling_rate = settings[10]
+        self.raw_active_dpi_presets = settings[11]
+        self.raw_enabled_dpi_presets = settings[12]
+        self.raw_dpi_values = [0] * 5
+        for i in range(5):
+            self.raw_dpi_values[i] = settings[13 + i]
+        self.raw_dpi_colors = [0] * 15
+        for i in range(15):
+            self.raw_dpi_colors[i] = settings[29 + i]
+
+        self.raw_current_lighting_effect = settings[53]
+
+        self.raw_colorful_streaming_speed = settings[54]
+        self.raw_colorful_streaming_direction = settings[55]
+
+        self.raw_steady_brightness = settings[56]
+        self.raw_steady_color = [0] * 3
+        for i in range(3):
+            self.raw_steady_color[i] = settings[57 + i]
+
+        self.raw_breathing_speed = settings[60]
+        self.raw_breathing_number_of_colors = settings[61]
+        self.raw_breathing_colors = [0] * 21
+        for i in range(21):
+            self.breathing_colors[i] = settings[62 + i]
+
+        self.raw_tail_speed = settings[83]
+
+        self.raw_neon_speed = settings[84]
+
+        self.raw_colorful_steady_colors = [0] * 15
+        for i in range(15):
+            self.raw_colorful_steady_colors[i] = settings[86 + i]
+
+        self.raw_streaming_speed = settings[117]
+
+        self.raw_wave_speed = settings[118]
+
 
     def make_package(self):
         pass
@@ -67,4 +105,5 @@ class M601:
 if __name__ == '__main__':
 
     mouse = M601()
-    mouse.change_mode(1)
+    mouse.read_settings()
+    mouse.parse_settings(mouse.settings_2)
